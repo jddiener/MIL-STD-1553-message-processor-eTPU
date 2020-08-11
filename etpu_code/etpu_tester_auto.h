cd092544766bbca30ce59052d99b7e9206fd44d9
jddiener <48527789+jddiener@users.noreply.github.com>
2020-08-11 08:34:44 -0600

    add the minimum eTPU build outputs (auto-generated API code) to support host code builds

---
+++ etpu_code/etpu_tester_auto.h
@@ -0,0 +1,15 @@
+
+#ifndef  __TESTER_CPU_H__ 
+#define  __TESTER_CPU_H__ 
+ 
+#define _FUNCTION_NUM_Tester_          11
+#define _ENTRY_TABLE_TYPE_Tester_             0 
+#define _FRAME_SIZE_Tester_               0x0018 
+// Function Variable Offsetts 
+#define _CPBA24_Tester_linkToChanNum_   0x0001
+#define _CPBA24_Tester_LatencyStart_      0x0005
+#define _CPBA24_Tester_LatencyDuration_   0x0009
+#define _CPBA24_Tester_LatencyPeriod_     0x000D
+#define _CPBA24_Tester_EncodedForcedState_       0x0011
+ 
+#endif // __TESTER_CPU_H__ 
